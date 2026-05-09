269e7704faf7346ace9b71e3a3c5086941cf73a7
sumowrestler99 <sumowrestler@gmail.com>
2026-05-08 23:37:28 -0700

    Add icons to which-key entries; add missing keys
    - Group icons: Find (magnify), Git (git), Debug (bug), Undo (restore),
      Neogen (comment-edit), Mason (package), Workspace (folder)
    - Individual key icons for LSP, diagnostics, file tree, save, paste image
    - Added missing entries: leader>ul, leader>eg, leader>eb, leader>dC
    - All icons use NF v3 U+F0xxx Material Design codepoints

--- lua/plugins/which_key.lua
+++ lua/plugins/which_key.lua
@@ -1,23 +1,24 @@
 return {
   'folke/which-key.nvim',
   event = "VeryLazy",
-  -- The 'config' function runs after the plugin is successfully loaded
   config = function()
     local wk = require("which-key")
 
     wk.add({
       -- LSP g-prefixed keys
-      { "gd",    desc = "LSP Definition" },
-      { "gD",    desc = "LSP Declaration" },
-      { "gi",    desc = "LSP Implementation" },
-      { "gr",    desc = "LSP References" },
-      { "K",     desc = "LSP Hover" },
-      { "<C-k>", desc = "LSP Signature Help" },
+      { "gd",    desc = "LSP Definition",       icon = { icon = "󰌹", color = "blue"   } },
+      { "gD",    desc = "LSP Declaration",       icon = { icon = "󰌹", color = "grey"   } },
+      { "gi",    desc = "LSP Implementation",    icon = { icon = "󰰱", color = "blue"   } },
+      { "gr",    desc = "LSP References",        icon = { icon = "󰈇", color = "blue"   } },
+      { "K",     desc = "LSP Hover",             icon = { icon = "󰋖", color = "cyan"   } },
+      { "<C-k>", desc = "LSP Signature Help",    icon = { icon = "󰊕", color = "cyan"   } },
+
       -- Treesitter incremental selection
       { "gnn", desc = "TS Init selection" },
       { "grn", desc = "TS Increment node" },
       { "grc", desc = "TS Increment scope" },
       { "grm", desc = "TS Decrement node" },
+
       -- Treesitter textobjects: select (visual + operator-pending)
       { "af", mode = { "x", "o" }, desc = "TS Around function" },
       { "if", mode = { "x", "o" }, desc = "TS Inside function" },
@@ -29,6 +30,7 @@ return {
       { "ix", mode = { "x", "o" }, desc = "TS Inside conditional" },
       { "al", mode = { "x", "o" }, desc = "TS Around loop" },
       { "il", mode = { "x", "o" }, desc = "TS Inside loop" },
+
       -- Treesitter textobjects: move
       { "]m", mode = { "n", "x", "o" }, desc = "TS Next function start" },
       { "]M", mode = { "n", "x", "o" }, desc = "TS Next function end" },
@@ -36,34 +38,48 @@ return {
       { "[M", mode = { "n", "x", "o" }, desc = "TS Prev function end" },
       { "]]", mode = { "n", "x", "o" }, desc = "TS Next class start" },
       { "[[", mode = { "n", "x", "o" }, desc = "TS Prev class start" },
+
       -- Treesitter textobjects: swap
       { "<leader>a", desc = "TS Swap next parameter" },
       { "<leader>A", desc = "TS Swap prev parameter" },
+
       -- Diagnostic navigation
-      { "[d", desc = "Prev Diagnostic" },
-      { "]d", desc = "Next Diagnostic" },
+      { "[d", desc = "Prev Diagnostic", icon = { icon = "󰒕", color = "yellow" } },
+      { "]d", desc = "Next Diagnostic", icon = { icon = "󰒕", color = "yellow" } },
+
       -- <leader> groups
-      { "<leader>u",  group = "Undo" },
+      { "<leader>u",  group = "Undo",      icon = { icon = "󰑖", color = "yellow" } },
       { "<leader>ut", desc = "Toggle undotree" },
-      { "<leader>f",  group = "Find" },
+      { "<leader>ul", desc = "Toggle layout guides (clean mode)", icon = { icon = "󰒉", color = "grey" } },
+
+      { "<leader>f",  group = "Find",      icon = { icon = "󰍉", color = "blue"   } },
       { "<leader>ft", desc = "Todo comments" },
-      { "]t", desc = "Next todo" },
-      { "[t", desc = "Prev todo" },
-      { "<leader>w",  desc = "Save file" },
-      { "<leader>e",  desc = "Toggle file tree" },
-      { "<leader>ef", desc = "Reveal file in tree" },
-      { "<leader>pi", desc = "Paste image" },
-      { "<leader>n",  group = "Neogen" },
+      { "]t", desc = "Next todo",  icon = { icon = "󰄲", color = "blue" } },
+      { "[t", desc = "Prev todo",  icon = { icon = "󰄲", color = "blue" } },
+
+      { "<leader>w",  desc = "Save file",        icon = { icon = "󰆓", color = "blue"   } },
+
+      { "<leader>e",  desc = "Toggle file tree",        icon = { icon = "󰙅", color = "green"  } },
+      { "<leader>ef", desc = "Reveal file in tree",     icon = { icon = "󰈞", color = "green"  } },
+      { "<leader>eg", desc = "Git status (float)",      icon = { icon = "󰊢", color = "orange" } },
+      { "<leader>eb", desc = "Toggle buffer list",      icon = { icon = "󰓩", color = "blue"   } },
+
+      { "<leader>pi", desc = "Paste image",             icon = { icon = "󰋩", color = "cyan"   } },
+
+      { "<leader>n",  group = "Neogen",    icon = { icon = "󰆉", color = "cyan"   } },
       { "<leader>nf", desc = "Generate annotation" },
-      { "<leader>g",  group = "Git" },
+
+      { "<leader>g",  group = "Git",       icon = { icon = "󰊢", color = "orange" } },
       { "<leader>gd", desc = "Diffview open" },
       { "<leader>gh", desc = "Diffview file history" },
       { "<leader>gH", desc = "Diffview repo history" },
       { "<leader>gx", desc = "Diffview close" },
-      { "<leader>d",  group = "Debug" },
+
+      { "<leader>d",  group = "Debug",     icon = { icon = "󰃤", color = "red"    } },
       { "<leader>db", desc = "Toggle breakpoint" },
       { "<leader>dB", desc = "Conditional breakpoint" },
       { "<leader>dc", desc = "Continue" },
+      { "<leader>dC", desc = "Run to cursor" },
       { "<leader>dn", desc = "Step over" },
       { "<leader>ds", desc = "Step into" },
       { "<leader>df", desc = "Step out" },
@@ -72,30 +88,34 @@ return {
       { "<leader>du", desc = "Toggle DAP UI" },
       { "<leader>dx", desc = "Terminate" },
       { "<leader>dK", desc = "Hover value" },
-      { "<leader>dC", desc = "Run to cursor" },
+
       -- Fn keys (active only during a DAP session)
       { "<F5>",  desc = "DAP Continue (session)" },
       { "<F9>",  desc = "DAP Toggle breakpoint (session)" },
       { "<F10>", desc = "DAP Step over (session)" },
       { "<F11>", desc = "DAP Step into (session)" },
       { "<F12>", desc = "DAP Step out (session)" },
+
       -- <space> LSP keys
-      { "<space>e",  desc = "Diagnostic float" },
-      { "<space>q",  desc = "Diagnostic loclist" },
+      { "<space>e",  desc = "Diagnostic float",         icon = { icon = "󰒕", color = "yellow" } },
+      { "<space>q",  desc = "Diagnostic loclist",        icon = { icon = "󰒕", color = "yellow" } },
       { "<space>D",  desc = "LSP Type Definition" },
-      { "<space>rn", desc = "LSP Rename" },
-      { "<space>ca", desc = "LSP Code Action" },
-      { "<space>f",  desc = "LSP Format buffer" },
-      { "<space>w",  group = "Workspace" },
+      { "<space>rn", desc = "LSP Rename",                icon = { icon = "󰑕", color = "blue"   } },
+      { "<space>ca", desc = "LSP Code Action",           icon = { icon = "󰌵", color = "yellow" } },
+      { "<space>f",  desc = "LSP Format buffer",         icon = { icon = "󰉼", color = "blue"   } },
+      { "<space>w",  group = "Workspace",  icon = { icon = "󰉋", color = "blue"   } },
       { "<space>wa", desc = "Add workspace folder" },
       { "<space>wr", desc = "Remove workspace folder" },
       { "<space>wl", desc = "List workspace folders" },
+
       -- Visual mode
       { "\\qf", mode = "v", desc = "LSP Range Format" },
+
       -- Mason
-      { "<leader>m",  group = "Mason" },
+      { "<leader>m",  group = "Mason",     icon = { icon = "󰏗", color = "purple" } },
       { "<leader>mi", "<cmd>Mason<cr>",       desc = "Open Mason UI" },
       { "<leader>mu", "<cmd>MasonUpdate<cr>", desc = "Update Mason registries" },
+
       -- Function keys
       { "<F4>", desc = "Toggle tab width 2/4" },
     })
